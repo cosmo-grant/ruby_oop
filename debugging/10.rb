1f06d5e65437b0fa37b1f5cabf0e87c49b7dfed3
Cosmo Grant <cosmodgrant@gmail.com>
2022-04-11 13:45:06 -0700

    Add solutions to Debugging

---
+++ debugging/10.rb
@@ -0,0 +1,42 @@
+class AuthenticationError < StandardError; end
+
+# A fake search engine
+# code omitted
+
+module DoesItRock
+  API_KEY = 'wrong key'
+
+  class NoScore; end
+
+  class Score
+    def self.for_term(term)
+      positive = SearchEngine.count(%{"#{term} rocks"}, API_KEY).to_f
+      negative = SearchEngine.count(%{"#{term} is not fun"}, API_KEY).to_f
+
+      positive / (positive + negative)
+    rescue ZeroDivisionError
+      NoScore.new
+    end
+  end
+
+  def self.find_out(term)
+    score = Score.for_term(term)
+
+    case score
+    when NoScore
+      "No idea about #{term}..."
+    when 0...0.5
+      "#{term} is not fun."
+    when 0.5
+      "#{term} seems to be ok..."
+    else
+      "#{term} rocks!"
+    end
+  rescue StandardError => e
+    e.message
+  end
+end
+
+puts DoesItRock.find_out('Sushi')       # Sushi seems to be ok...
+puts DoesItRock.find_out('Rain')        # Rain is not fun.
+puts DoesItRock.find_out('Bug hunting') # Bug hunting rocks!
